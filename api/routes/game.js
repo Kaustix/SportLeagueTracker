a2ca2d7cdc858e9f53d2068a7a6ccee802f61c43
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2016-11-21 14:58:33 -0700

    converted mongoose calls to promises

--- api/routes/game.js
+++ api/routes/game.js
@@ -6,71 +6,42 @@ const router = express.Router();
 
 // Get all
 router.get('/', (req, res) => {
-  Game.find({}, (err, games) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    res.json(games);
-  })
+  Game.find()
+    .then(games => res.json(games))
+    .catch(err => {res.send(err)});
 });
 
 // Add new
 router.post('/', (req, res) => {
   const game = new Game(req.body);
-
-  game.save((err) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    res.send({message: 'Success'});
-  });
+  game.save()
+    .then(() => res.send({message: 'success'}))
+    .catch(err => res.send(err));
 });
 
 // Get by id
 router.get('/:id', (req, res) => {
-  Game.findOne({_id: req.params.id}, (err, game) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    if (!game) {
-      res.json({message: "No game found"});
-    }
-
-    _.extend(game, req.body);
-
-    game.save((err) => {
-      if (err) {
-        res.send(err);
-      }
-
-      res.json({message: 'Success'});
-    });
-  });
+  Game.findOne({_id: req.params.id})
+    .then(game => {
+      if (!game) res.json({message: "no game found"});
+      _.extend(game, req.body);
+      game.save().then(res.json({message: "success"})).catch(err => res.send(err));
+    })
+    .catch(err => res.send(err));
 });
 
 // Update by id
 router.put('/:id', (req, res) => {
-  Game.findOne({ _id: req.params.id}, (err, game) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    res.json(game);
-  });
+  Game.findOne({ _id: req.params.id})
+    .then(game => res.json(game))
+    .catch(err => res.send(err));
 });
 
 //delete by id
 router.put('/:id', (req, res) => {
-  Game.remove({ _id: req.params.id}, (err, game) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    res.json({ message: 'Success'});
-  });
+  Game.remove({ _id: req.params.id})
+    .then(res.send({message: 'success'}))
+    .catch(err => res.send(err))
 });
 
 export default router;
