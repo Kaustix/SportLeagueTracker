0c8ab3a57db2a58c30f2d439bbe78b39b3018e2a
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2016-11-21 17:20:56 -0700

    Added a models index, and moved routes away from express

--- api/routes/game.js
+++ api/routes/game.js
@@ -1,26 +1,20 @@
-import express from 'express';
-import Game from '../models/game'
+import { Game } from '../models'
 import _ from 'underscore';
 
-const router = express.Router();
-
-// Get all
-router.get('/', (req, res) => {
+export function all(req, res) {
   Game.find()
     .then(games => res.json(games))
     .catch(err => {res.send(err)});
-});
+}
 
-// Add new
-router.post('/', (req, res) => {
+export function create(req, res) {
   const game = new Game(req.body);
   game.save()
     .then(() => res.send({message: 'success'}))
     .catch(err => res.send(err));
-});
+}
 
-// Get by id
-router.get('/:id', (req, res) => {
+export function get(req, res) {
   Game.findOne({_id: req.params.id})
     .then(game => {
       if (!game) res.json({message: "no game found"});
@@ -28,20 +22,16 @@ router.get('/:id', (req, res) => {
       game.save().then(res.json({message: "success"})).catch(err => res.send(err));
     })
     .catch(err => res.send(err));
-});
+}
 
-// Update by id
-router.put('/:id', (req, res) => {
+export function update(req, res) {
   Game.findOne({ _id: req.params.id})
     .then(game => res.json(game))
     .catch(err => res.send(err));
-});
+}
 
-//delete by id
-router.put('/:id', (req, res) => {
+export function remove(req, res) {
   Game.remove({ _id: req.params.id})
     .then(res.send({message: 'success'}))
     .catch(err => res.send(err))
-});
-
-export default router;
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import GameRoutes from './game';
+import * as GameRoutes from './game';
 
 const router = express.Router();
 
-// index
+// Index
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+res.send('Hello World!');
 });
 
-router.use('/games', GameRoutes);
+router.route('/games').get(GameRoutes.all).post(GameRoutes.create);
+router.route('/games/:id').get(GameRoutes.get).put(GameRoutes.update).delete(GameRoutes.remove);
 
 export default router;
 
